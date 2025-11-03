a4351ce1601d7f866180ddba46ef360367dc2836
clrnc01001010 <52bbkrgkwk@privaterelay.appleid.com>
2025-11-03 10:39:25 +0400

    Add files via upload

---
+++ Exe_7.py
@@ -0,0 +1,19 @@
+print("0 to 50:")
+for x in range(0, 51, 1):
+    print(x)
+
+print("50 to 0:")
+for x in range(50, -1, -1):
+    print(x)
+
+print("30 to 50:")
+for x in range(30, 51, 1):
+    print(x)
+
+print("50 to 10 (by 2s):")
+for x in range(50, 9, -2):
+    print(x)
+
+print("100 to 200 (by 5s):")
+for x in range(100, 201, 5):
+    print(x)
